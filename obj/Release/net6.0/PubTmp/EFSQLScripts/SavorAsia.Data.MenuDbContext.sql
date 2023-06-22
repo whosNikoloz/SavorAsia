419586f51a4f780e643981c6eddc9d7cbbd99f92
whosNikoloz <nika.kobaidze04@gmail.com>
2023-06-22 15:56:28 +0400

    LastUpdate

---
+++ obj/Release/net6.0/PubTmp/EFSQLScripts/SavorAsia.Data.MenuDbContext.sql
@@ -0,0 +1,47 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230610172147_MenuV3')
+BEGIN
+    ALTER TABLE [Rice] ADD [CategoryId] int NOT NULL DEFAULT 0;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230610172147_MenuV3')
+BEGIN
+    ALTER TABLE [Ramen] ADD [CategoryId] int NOT NULL DEFAULT 0;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230610172147_MenuV3')
+BEGIN
+    ALTER TABLE [Noodles] ADD [CategoryId] int NOT NULL DEFAULT 0;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230610172147_MenuV3')
+BEGIN
+    ALTER TABLE [Drinks] ADD [CategoryId] int NOT NULL DEFAULT 0;
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230610172147_MenuV3')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230610172147_MenuV3', N'6.0.16');
+END;
+GO
+
+COMMIT;
+GO
+
---
+++ obj/Release/net6.0/PubTmp/EFSQLScripts/SavorAsia.Data.OrderDbContext.sql
@@ -0,0 +1,23 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230614171648_OrderFinalV1')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230614171648_OrderFinalV1', N'6.0.16');
+END;
+GO
+
+COMMIT;
+GO
+
